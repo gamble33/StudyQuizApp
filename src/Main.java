adcee517db2b9ef6bc0bd3701b2c3dc71378bd31
George Novachuk <giafilmsyt@gmail.com>
2021-10-21 18:51:22 +0200

    Remove Font Declaration in application.fxml

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,8 @@ import javafx.stage.Stage;
 import models.MultipleChoiceQuestion;
 import views.MultipleChoiceView;
 
+import java.util.Objects;
+
 
 public class Main extends Application {
 
@@ -13,16 +15,19 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("resources/application.fxml"));
-        String styleSheet = getClass().getResource("./resources/application.css").toExternalForm();
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("resources/application.fxml")));
+        String styleSheet = Objects.requireNonNull(getClass().getResource("./resources/application.css")).toExternalForm();
         primaryStage.setTitle("Study Quiz");
         scene = new Scene(root, 1280, 720);
         scene.getStylesheets().add(styleSheet);
         primaryStage.setScene(scene);
         primaryStage.show();
 
-        MultipleChoiceQuestion q = new MultipleChoiceQuestion("1+1?", new String[]{"1", "Goose", "Sudan", "South Asia", "2", "3", "5", "Fourteen", "China", "Yes", "Maybe", "No"}, new Integer[]{4});
-        new MultipleChoiceView(q).show(scene);
+        MultipleChoiceQuestion q1 = new MultipleChoiceQuestion("1+1?", new String[]{"1", "Goose", "Sudan", "South Asia", "2", "3", "5", "Fourteen", "China", "Yes", "Maybe", "No"}, new Integer[]{4});
+        new MultipleChoiceView(q1).show(scene);
+        MultipleChoiceQuestion q2 = new MultipleChoiceQuestion("2+3?", new String[]{"1", "Goose", "Sudan", "South Asia", "2", "3", "5", "Fourteen", "China", "Yes", "Maybe", "No"}, new Integer[]{4});
+//        new MultipleChoiceView(q2).show(scene);
+
 
     }
 
